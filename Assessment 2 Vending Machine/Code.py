9a1f3ff323ea32bc275c2714f8d10e74d9e34a85
Anzin-Maglente <144773742+AnzinMaglente@users.noreply.github.com>
2023-12-19 17:21:27 +0400

    Update Code.py

--- Assessment 2 Vending Machine/Code.py
+++ Assessment 2 Vending Machine/Code.py
@@ -25,19 +25,18 @@ current_money = 0.0
 change_money = 0.0
 #Above are variables that will be used later.
 
-input("""Hello, welcome to the vending machine. This program was made by Anzin R. Maglente, 
-if you find any errors please message him. Now without further adio, let us begin.\n""")
+input("""Hello, welcome to the vending machine. This program was made by Anzin R. Maglente, if you find any errors please message him. Now without further adio, let us begin.\n""")
 
 #This is a comment:
 """The while loop below allows the program to loop until the user is satisfied with
 their selections, it allows for multiple products to be selected from the vending machine."""
 
-while check == False:
+while check == False: #This restarts the loop when it reaches the end unless the user chooses to buy another product.
     product = {
             'amount' : 0,
-            'item' : 'b',
+            'item' : '',
             'price' : 0.0,
-            'type' : 'd',
+            'type' : '',
             'bundle_item' : 'None',
             }
     #This is the product's values that are impacted by the user's input.
@@ -52,18 +51,74 @@ while check == False:
     product_id = input("Please input the product ID, you would want to buy: ")
     #This allows the user to select an item to add to their shopping cart.
     
-    definitions.product_id_assurance(product_id)
+    #This if conditions changes the product id to it's right id if the user made a mistake in their grammar
+    if product_id == "c1": 
+        product_id = "C1"
+
+    elif product_id == "c2":
+        product_id = "C2"
+
+    elif product_id == "c3":
+        product_id = "C3"
+
+    elif product_id == "c4":
+        product_id = "C4"
+
+    elif product_id == "ch1" or product_id == "CH1":
+        product_id = "Ch1"
+
+    elif product_id == "ch2" or product_id == "CH2":
+        product_id = "Ch2"
+
+    elif product_id == "ch3" or product_id == "CH3":
+        product_id = "Ch3"
+
+    elif product_id == "p1":
+        product_id = "P1"
+
+    elif product_id == "p2":
+        product_id = "P2"
+
+    elif product_id == "p3":
+        product_id = "P3"
+
+    elif product_id == "d1":
+        product_id = "D1"
+
+    elif product_id == "d2":
+        product_id = "D2"
+
+    elif product_id == "d3":
+        product_id = "D3"
+
+    elif product_id == "sd1" or product_id == "Sd1":
+        product_id = "SD2"
+
+    elif product_id == "sd2" or product_id == "Sd2":
+        product_id = "SD2"
+
+    elif product_id == "sd3" or product_id == "Sd3":
+        product_id = "SD3"
+
+    elif product_id == "j1":
+        product_id = "J1"
+
+    elif product_id == "j2":
+        product_id = "J2"
+
+    elif product_id == "j3":
+        product_id = "J3"
     
     cur = conn.cursor()
     qry = cur.execute("SELECT product_id FROM Item_Database")
     res = qry.fetchall()
     database_list = [x[0] for x in res]
-    if product_id in database_list:
+    if product_id in database_list: #This checks if the selected product still has a stock inside the database
         print("")
         cur = conn.cursor()
         qry = cur.execute("SELECT amount, item, price, type FROM Item_Database WHERE product_id=?", (product_id,))
         res = qry.fetchone()
-        product['amount'] = res[0]
+        product['amount'] = res[0] #This changes the value of the dictionary named "product"
         if product['amount'] > 0:
             product['item'] = res[1]
             product['price'] = res[2]
@@ -72,13 +127,14 @@ while check == False:
             print ("You added " + product['item'] + " to your cart, it cost " + str(product['price']) + " and it is in the " + product['type'] + " category.")
         else:
             input("Sorry, but the item you selected ran out. Please choose again. ")
+            print("")
             continue
     else:
         input("Sorry, our system did not recognize your answer please try again. ")
+        print("")
         continue
-    #
 
-    if product['item'] == 'Doritos Nacho Cheese' or product['item'] == 'Sprite':
+    if product['item'] == 'Doritos Nacho Cheese' or product['item'] == 'Sprite': #This checks if the product has a bundle or not. Along the rest
         if product['item'] == 'Doritos Nacho Cheese':
             previous_product_id = product_id
             p1 = product['item']
@@ -255,12 +311,28 @@ while check == False:
             definitions.bundle_check(product_id, product, previous_product_id, p1, p2)
     else:
         print ("There does not seem to be a bundle associated with your purchase")
+    
+    remove_check = False
+    while remove_check == False:
+        remove = input('Do you want to continue with your order? Please write "Yes" or "No": ')
+        if remove == 'Yes'  or remove == 'yes' or remove == 'YES' or remove == 'y':
+            remove_check = True
+        elif remove == "No" or remove == 'no' or remove == 'NO' or remove == 'n':
+            remove_check = True
+        else:
+            print("Sorry, our system did not recognize your answer please try again.\n")
+    
+    if remove == 'No':
+        definitions.increase_amount(product_id)
+        product_id = previous_product_id
+        definitions.increase_amount(product_id)
+        continue
 
-    shopping_cart.append(product)
+    shopping_cart.append(product) #This adds the product details to the shopping_cart
     
     continue_check = False
-    while continue_check == False:
-        continue_answer = input('Do you want to continue your shopping? Please enter "Yes" or "No" ')
+    while continue_check == False: #This allows the user to buy another product or move on to the next step
+        continue_answer = input('\nDo you want to continue your shopping? Please enter "Yes" or "No": ')
         if continue_answer == 'Yes' or continue_answer == 'yes' or continue_answer == 'YES' or continue_answer == 'y':
             continue_check = True
         elif continue_answer == 'No' or continue_answer == 'no' or continue_answer == 'NO' or continue_answer == 'n':
@@ -269,7 +341,7 @@ while check == False:
         else:
             print("Sorry, our system did not recognize your answer please try again.\n")
 
-for product in shopping_cart:
+for product in shopping_cart: #This gets the total price by checking each product's price
     current_price = (product['price'])
     total_price += current_price
 
@@ -278,13 +350,13 @@ input("\nProcessing order...\n")
 print("Your total purchase is " + str(total_price) + " dhs\n")
 definitions.show_product(shopping_cart)
 
-while money_check == False:
-    if current_money >= total_price:
+while money_check == False: #This loops back until the right amount of coins is inserted
+    if current_money >= total_price: #This checks if the amount of money inputted is higher than the total price
         change_money = current_money - total_price
-        if change_money == 0:
+        if change_money == 0: #This checks if the change is equal to zero
             print("\n~~ A sound is heard in the opening beneath ~~\n")
             print("Here are your products:")
-            for product in shopping_cart:
+            for product in shopping_cart: #This shows off your shopping cart again for the final tunme
                 if product['type'] == "bundle":
                     print("\t" + product['item'] + " and " + product['bundle_item'])
                 else:
@@ -317,9 +389,9 @@ while money_check == False:
             else:
                 print("Sorry, our system did not recognize your answer please try again.\n")
 
-    else:
-        print("\nThe money you have currently inputed: " + current_money)
-        print("Total price: " + str(total_price))
+    else: #This allows the user to add money to their balance
+        print("\nYour current balance is: " + str(current_money) + " dhs")
+        print("Total price: " + str(total_price) + " dhs")
         money = input("\nPlease input either 25 fils, 50 fils, 1 dhs, 5 dhs, 10 dhs, or 20 dhs into the machine to get your ordered products: ")
         if money == ("25 fils") or money == ("25 Fils") or money == ("0.25"):
             current_money += 0.25
